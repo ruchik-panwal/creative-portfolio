0930dfaff62eb4ea28ef25abe6fbe120f7a3e5a7
ruchik-panwal <ruchik.panwal@gmail.com>
2026-03-03 09:58:01 +0000

    hmmmmmmmmmm

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,6 +1,6 @@
 import Hero from "./components/Hero";
 import Footer from "./components/Footer";
-import WorkSection from "./components/WorkSection";
+import WorkSection from "./components/MyWorkSection/WorkSection";
 import SmoothScroll from "./components/SmoothScroll"; // Make sure this path matches where you saved the file
 
 export default function Home() {
